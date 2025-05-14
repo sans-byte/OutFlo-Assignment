e88da236a086fca4faae1f414c8fb68548d36b1c
sanskar jain <sanskarj15@gmail.com>
2025-05-14 16:39:54 +0530

    Resolve Cors

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,21 @@ const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/outflo";
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://out-flo-assignment-six.vercel.app/",
+      "http://out-flo-assignment-six.vercel.app/",
+      "https://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://127.0.0.1:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // Routes
